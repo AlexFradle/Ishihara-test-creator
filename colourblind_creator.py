759dbbf50044c9ccbec6835ba94d83beb1fbed71
AlexFradle <42185721+AlexFradle@users.noreply.github.com>
2020-03-01 21:04:08 +0000

    Add files via upload

---
+++ colourblind_creator.py
@@ -0,0 +1,106 @@
+import pygame
+from random import randint, choice
+import threading
+from PIL import Image
+from typing import Callable
+
+
+class Dot:
+    def __init__(self, x, y, radius, colour):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.colour = colour
+
+
+class Dots:
+    def __init__(self, cc: int, u: int, l: int, optn: int, func: Callable[[int, int], bool]=None, radius: int=350, img_name: str="read_img.bmp"):
+        """
+        Constructor
+        :param cc: The amount of circles to draw
+        :param u: Highest radius of dot
+        :param l: Lowest radius of dot
+        :param optn: Type of blindness colours
+        :param func: Graph function to run coords through - default if circles function
+        :param radius: Radius of main circle or width of shape if not circle
+        """
+        self.circle_count = cc
+        # Randomly generating each radius
+        self.radi = [randint(l, u) for _ in range(self.circle_count)]
+        self.dots = []
+        self.radius = radius
+        # Graph function
+        self.func = (lambda x1, y1: (x1 - 360) ** 2 + (y1 - 360) ** 2 <= self.radius ** 2) if func is None else func
+        assert isinstance(self.func(0, 0), bool), "function didn't return boolean"
+        # Loading image
+        img = Image.open(img_name)
+        pixels = img.load()
+        # Creating pixel list
+        self.img_coords = [[pixels[i, j] for j in range(720)] for i in range(720)]
+        self.percentage = 0
+        self.optn = optn
+        # Starting make_dots thread to do task in background
+        threading.Thread(target=self.make_dots, daemon=True).start()
+
+    def make_dots(self) -> bool:
+        """
+        Threaded function to create colour dots
+        :return: Bool to stop thread when done
+        """
+        # Colour dict for each blindness type
+        colours = {
+            "red-green": {
+                "outer": [
+                    (196, 118, 67), (230, 168, 115), (179, 114, 86), (211, 152, 119), (233, 150, 103), (197, 121, 71)
+                ],
+                "inner": [
+                    (65, 171, 111), (191, 188, 109), (136, 146, 77), (193, 186, 109), (146, 186, 119), (170, 177, 115)
+                ]
+            },
+            "green-red": {
+                "outer": [
+                    (65, 171, 111), (191, 188, 109), (136, 146, 77), (193, 186, 109), (146, 186, 119), (170, 177, 115)
+                ],
+                "inner": [
+                    (196, 118, 67), (230, 168, 115), (179, 114, 86), (211, 152, 119), (233, 150, 103), (197, 121, 71)
+                ]
+            },
+            "blue-yellow": {
+                "outer": [
+                    (147, 160, 140), (60, 74, 70), (101, 111, 103), (150, 162, 140), (107, 114, 107), (93, 100, 93)
+                ],
+                "inner": [
+                    (216, 152, 127), (212, 131, 105), (216, 73, 77), (133, 86, 96), (196, 134, 130), (214, 155, 141)
+                ]
+            }
+        }
+        # Assigning which blindness option to use
+        optn = [i[1] for i in enumerate(colours) if i[0] == self.optn - 1][0]
+        count = 0
+        # Loop to randomly make circle fit inside bounds
+        while True:
+            # Allowed 10 pixels each side to adjust for bigger radii
+            x = randint(10, (self.radius * 2) - 10)
+            y = randint(10, (self.radius * 2) - 10)
+            # If coords satisfy func
+            if self.func(x, y):
+                # Checking circle collision
+                n = pygame.Rect(0, 0, self.radi[count] * 2, self.radi[count] * 2)
+                n.center = (x, y)
+                able = []
+                for p, i in enumerate(self.dots):
+                    r = pygame.Rect(0, 0, self.radi[p] * 2, self.radi[p] * 2)
+                    r.center = (i.x, i.y)
+                    if r.colliderect(n):
+                        able.append(False)
+                    else:
+                        able.append(True)
+                # If circle doesnt collide with anything it is allowed
+                if all(able):
+                    col = choice(colours[optn]["inner"]) if self.img_coords[x][y] == 0 else choice(colours[optn]["outer"])
+                    self.dots.append(Dot(x, y, self.radi[count], col))
+                    count += 1
+                    self.percentage = len(self.dots) / self.circle_count
+            # If all circles are placed the break
+            if count == self.circle_count:
+                return False
---
+++ colourblind_gui.py
@@ -0,0 +1,89 @@
+import pygame
+from colourblind_creator import Dots
+from tkinter import *
+pygame.init()
+
+
+class SettingsWindow(Frame):
+    def __init__(self, master: Tk):
+        """
+        Constructor
+        :param master: Tk for which window is created on
+        """
+        # Creating window and calling create_widgets func
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.iv = IntVar()
+        self.create_widgets()
+
+    def get_radio(self) -> None:
+        """
+        Called when start button is pressed
+        :return: None
+        """
+        # Only quits if a radio button has been pressed
+        if self.iv.get():
+            self.master.destroy()
+
+    def create_widgets(self) -> None:
+        """
+        Creates radio buttons and start button
+        :return: None
+        """
+        label = Label(self, text="Choose type: ")
+        label.pack()
+        # Loops to create radio buttons
+        for txt, v in [("red-green", 1), ("green-red", 2), ("blue-yellow", 3)]:
+            r = Radiobutton(self, text=txt, variable=self.iv, value=v)
+            r.pack()
+        button = Button(self, text="START", command=self.get_radio)
+        button.pack()
+
+
+def settings():
+    root = Tk()
+    root.title("Settings")
+    app = SettingsWindow(root)
+    app.mainloop()
+    return app.iv.get()
+
+
+# Called to choose which blindness option
+choice = settings()
+
+# Creating main window and misc variables
+width, height = 820, 720
+display = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+running = True
+load_bar_outer = pygame.Rect(780, 10, 30, 700)
+
+# Initiates Dots class to start making dots to be drawn
+dots = Dots(2000, 5, 2, choice)
+
+# Main game loop
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+    display.fill((255, 255, 255))
+
+    # Draws dots
+    for p, dot in enumerate(dots.dots):
+        pygame.draw.circle(display, dot.colour, (dot.x, dot.y), dot.radius)
+
+    # Draws loading bar
+    pygame.draw.rect(display, (0, 0, 0), load_bar_outer)
+    pygame.draw.rect(display, (0, 255, 0), pygame.Rect(785, 15, 20, 690 * dots.percentage))
+
+    pygame.display.update()
+    clock.tick(60)
+
+
+pygame.quit()
